7a4445e1e1ba7892cec988209703638295113172
Thibault Leruitte <thibault@leruitte.name>
2014-02-28 14:25:05 +0100

    Simpler method to add css style to page

--- src/utils.js
+++ src/utils.js
@@ -200,25 +200,7 @@
         return $.isArray(val) ? val : [val];
     };
 
-    // http://paulirish.com/2008/bookmarklet-inject-new-css-rules/
     addCSS = function(css) {
-        var tag, iefail;
-        if (document.createStyleSheet) {
-            try {
-                document.createStyleSheet().cssText = css;
-                return;
-            } catch (e) {
-                // IE <= 9 maxes out at 31 stylesheets; inject into page instead.
-                iefail = true;
-            }
-        }
-        tag = document.createElement('style');
-        tag.type = 'text/css';
-        document.getElementsByTagName('head')[0].appendChild(tag);
-        if (iefail) {
-            document.styleSheets[document.styleSheets.length - 1].cssText = css;
-        } else {
-            tag[(typeof document.body.style.WebkitAppearance == 'string') /* webkit only */ ? 'innerText' : 'innerHTML'] = css;
-        }
+        $(document).find('head').append('<style type="text/css">@charset "UTF-8";'+ css +'</style>');
     };
 
